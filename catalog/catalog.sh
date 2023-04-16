ce28dbfc7f62fed9089afd27fe1c9f64728b5469
lintocjose22 <lintocjose22@gmail.com>
2023-04-16 13:16:33 -0400

    after updating catalog

---
+++ catalog/catalog.sh
@@ -0,0 +1,12 @@
+sudo curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+sudo yum install nodejs -y
+sudo useradd roboshop
+sudo mkdir /app
+sudo curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cd /app
+sudo unzip /tmp/catalogue.zip
+cd /app
+sudo npm install
+sudo systemctl daemon-reload
+sudo systemctl enable catalogue
+sudo systemctl start catalogue
